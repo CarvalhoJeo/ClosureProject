2c4452ead41a797a5ebdf9b516338b937ec30f83
Carvalho <75193903+CarvalhoJeo@users.noreply.github.com>
2021-05-18 23:21:10 -0300

    Update in project
    It still don't subtract people in the room

--- ClosureProject/ClosureProject.ino
+++ ClosureProject/ClosureProject.ino
@@ -5,26 +5,26 @@ void setup() {
 }
 
 void loop() {
-  int sensorFora = digitalRead(8), sensorDentro = digitalRead(7), pessoasSala = 0;
+  int sensorFora = digitalRead(8), sensorDentro = digitalRead(7);
+  static int pessoasSala = 0, c;
 
-  delay(1000);
+  delay(1000); 
   if(sensorFora < 1){
-    if(sensorDentro < 1){
-      pessoasSala++;
-      Serial.println("Pessoas na sala:");
-      Serial.println(pessoasSala);
-    }else{
-      Serial.println("Pessoas na sala:");
-      Serial.println(pessoasSala);
-    }
-    }else if(sensorDentro < 1){
-      if(sensorFora < 1){
-      pessoasSala--;
-      Serial.println("Pessoas na sala:");
-      Serial.println(pessoasSala);
-    }else{
-      Serial.println("Pessoas na sala:");
-      Serial.println(pessoasSala);
-    }
+    c = 1;
   }
-}
+  if(sensorDentro < 1 && c == 1){
+    pessoasSala++;
+    Serial.println("Pessoas na sala:");
+    Serial.println(pessoasSala);
+    c = 0;
+    } 
+  if(sensorDentro < 1){
+    c = 2;
+  }
+  if(sensorFora < 1 && c == 2){
+    pessoasSala++;
+    Serial.println("Pessoas na sala:");
+    Serial.println(pessoasSala);
+    c = 0;
+  }
+ }
