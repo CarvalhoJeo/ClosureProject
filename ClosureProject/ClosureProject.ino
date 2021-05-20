b31d8ceb766e3a1974f751979dd15a9b6ee942f2
Carvalho <75193903+CarvalhoJeo@users.noreply.github.com>
2021-05-19 22:38:24 -0300

    Update on the algorithm and comments added
    The algorithm still isn't subtracting people from the variable

--- ClosureProject/ClosureProject.ino
+++ ClosureProject/ClosureProject.ino
@@ -1,28 +1,38 @@
 void setup() {
+  //Inicia o monitor serial
   Serial.begin(9600);
+  //Adiciona os sensores como INPUT
   pinMode(8, INPUT);
   pinMode(7, INPUT);
 }
 
 void loop() {
+  //Cria variaveis para saber quais sensores são quais
   int sensorFora = digitalRead(8), sensorDentro = digitalRead(7);
+  //Criar duas variaveis estáticas para não cairem no loop
   static int pessoasSala = 0, c;
 
-  delay(1000); 
+  delay(100);
+
+  //Vê se o sensor de fora foi detectado se for da 1 para a variavel de controle
   if(sensorFora < 1){
     c = 1;
   }
+  
+  //Depois vê se o sensor de dentro vai ser detectado logo depois se for, ele vai adicionar uma pessoas pois primeiro passou pelo sensor de fora
   if(sensorDentro < 1 && c == 1){
     pessoasSala++;
     Serial.println("Pessoas na sala:");
     Serial.println(pessoasSala);
     c = 0;
-    } 
+    }
+
+  //NÃO FUNCIONANDO!!!
   if(sensorDentro < 1){
     c = 2;
   }
   if(sensorFora < 1 && c == 2){
-    pessoasSala++;
+    pessoasSala--;
     Serial.println("Pessoas na sala:");
     Serial.println(pessoasSala);
     c = 0;
